3dd193c6cc75a35c24eea0a2025dbc544a0d065e
Thomas Valette <valettethomas2@gmail.com>
2012-10-25 16:41:06 +0200

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,20 +9,27 @@ module SessionsHelper
   end
 
   def current_user=(user)
-	@current_user = user
+	  @current_user = user
   end
 	
   def current_user
-	@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	  @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
   def current_user?(user)
     user == current_user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
   def sign_out
-	self.current_user = nil
-	cookies.delete(:remember_token)
+  	self.current_user = nil
+  	cookies.delete(:remember_token)
   end
 
   def redirect_back_or(default)
